492fd121ff8dea165627a45fe14d3a23275be29d
Hukasx0 <hubertkasp13@gmail.com>
2023-02-04 19:59:48 +0100

    Added screenshot button

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const fs = require('fs');
-const io = require('socket.io');
 const screenshot = require('screenshot-desktop');
 
 const hostname = '127.0.0.1';
@@ -8,9 +7,45 @@ const port = 3000;
 
 const dispWidth = 1920;
 const dispHeight  = 1080;
-const updateMs = 400;
+const updateMs = 120;
 
 var buffer;
+
+class Streamer {
+    constructor(ioCon, id, width, height, ms){
+        this.id = id;
+        this.ioCon = ioCon;
+        this.width = width;
+        this.height = height;
+        this.ms = ms;
+    }
+    replaceTags(file){
+        return file.replace(`<WebScreenJS id="${this.id}.ssBtn"></WebScreenJS>`,`
+        <button id="${this.id}.ssBtn">screenshot</button>
+        <script>
+        const ssBtn = document.getElementById('${this.id}.ssBtn');
+        ssBtn.addEventListener("click", function() {
+            fetch('/ss', { method: 'POST' })
+            .then(response => response.json())
+            .then(data => {
+                window.location.href = data.ss;
+            })
+            .catch(error => console.error(error))
+        });
+        </script>
+        `
+        ).replace(`<WebScreenJS id="${this.id}.streamer"></WebScreenJS>`,`
+        <img id="${this.id}.streamer" src="" width="${this.width}" height="${this.height}">
+        <script src="${this.ioCon}socket.io/socket.io.js"></script>
+        <script>
+            const image = document.getElementById('${this.id}.streamer');\nconst socket = io('${this.ioCon}');
+            socket.on('update', data => {
+                image.src = data;
+            });
+        </script>`
+        );
+    }
+}
 
 function getImgBuffer() {
     return screenshot({format: 'jpeg'})
@@ -23,6 +58,11 @@ function getImgBuffer() {
 }
 
 const server = http.createServer((req, res) => {
+    if(req.method === 'POST' && req.url === "/ss"){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({ ss: buffer}));
+    }
+    else{
     fs.readFile('index.html', 'utf8', (err, index) => {
         if (err) {
             console.error(err);
@@ -30,13 +70,21 @@ const server = http.createServer((req, res) => {
             res.end("Server error");
         }
         else {
-            const indexMod = index.replace('{dispWidth}',dispWidth).replace('{dispHeight}',dispHeight);
+            const stream = new Streamer("http://localhost:3000/","stream",dispWidth,dispHeight,updateMs);
+            const indexMod = stream.replaceTags(index);
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
             res.end(indexMod);
             
         }
     })
+}
+});
+
+const sc = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
 });
 
 setInterval(function(){
@@ -45,7 +93,6 @@ setInterval(function(){
     });
 },updateMs);
 
-const sc = io(server);
 sc.on('connection', socket => {
     setInterval(function(){
         socket.emit('update',buffer);
